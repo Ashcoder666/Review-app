d71a8142ce01cda9583f9b3e48d46c4d36ee69eb
ashir <ashirnbasheer@gmail.com>
2022-02-15 11:39:43 +0530

    initial commit

---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+
+    {
+        id:1,
+        name:'Aaaaaa',
+        job:'web Devlepor',
+        image:'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
+        review:'hwq3rioj3wop1kipkijikdfewhrifhewiorojrfjweirfwiejhifhweohfduiwehfuidhweijhfiweufiweirfewihjrjneajfdhahfiouwejrfeifwjiofdriwqhrjoeqrijwioejriowqjerdi'
+    },
+    {
+        id:2,
+        name:'Bbbbbb',
+        job:'web designer',
+        image:'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
+        review:'hwq3rioj3wop1kipkijikdfewhrifhewiorojrfjweirfwiejhifhweohfduiwehfuidhweijhfiweufiweirfewihjrjneajfdhahfiouwejrfeifwjiofdriwqhrjoeqrijwioejriowqjerdi'
+    },
+    {
+        id:3,
+        name:'Ccccc',
+        job:'sys administrator',
+        image:'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
+        review:'hwq3rioj3wop1kipkijikdfewhrifhewiorojrfjweirfwiejhifhweohfduiwehfuidhweijhfiweufiweirfewihjrjneajfdhahfiouwejrfeifwjiofdriwqhrjoeqrijwioejriowqjerdi'
+    },
+    {
+        id:4,
+        name:'Dddddd',
+        job:'CEO',
+        image:'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
+        review:'hwq3rioj3wop1kipkijikdfewhrifhewiorojrfjweirfwiejhifhweohfduiwehfuidhweijhfiweufiweirfewihjrjneajfdhahfiouwejrfeifwjiofdriwqhrjoeqrijwioejriowqjerdi'
+    },
+    {
+        id:5,
+        name:'me',
+        job:'jobless',
+        image:'https://png.pngtree.com/png-vector/20190710/ourmid/pngtree-user-vector-avatar-png-image_1541962.jpg',
+        review:'hwq3rioj3wop1kipkijikdfewhrifhewiorojrfjweirfwiejhifhweohfduiwehfuidhweijhfiweufiweirfewihjrjneajfdhahfiouwejrfeifwjiofdriwqhrjoeqrijwioejriowqjerdi'
+    }
+
+]
